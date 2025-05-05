b7fea57356f51a5c8a5654b6488726a6fbfe0d18
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-05-05 03:31:44 +0200

    Added Moveable.java and implemented abstract methods

---
+++ src/main/java/at/fhj/msd/Movable.java
@@ -0,0 +1,13 @@
+package at.fhj.msd;
+
+public abstract class Movable {
+
+    public void moveUp() {}
+
+    public void moveDown() {}
+
+    public void moveLeft() {}
+
+    public void moveRight() {}
+    
+}
