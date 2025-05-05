a5b9b30a8b519e39382ffd9b5af9a65bc77540fd
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-05-05 03:36:01 +0200

    Implemented MovablePoint class with basic attributes and constructor

---
+++ src/main/java/at/fhj/msd/MovablePoint.java
@@ -0,0 +1,18 @@
+package at.fhj.msd;
+
+public class MovablePoint extends Movable {
+
+    protected int x;
+    protected int y;
+    protected int xSpeed;
+    protected int ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+
+}
